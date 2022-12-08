3c74409e72842a17faf7fd1a51716898538d8c50
greendar <dwgreen@hwdsb.on.ca>
2022-12-08 15:56:24 -0500

    added 2022-23 folder

--- kiwiDriveCode.java
+++ kiwiDriveCode.java
@@ -2,15 +2,15 @@
 
 
 public void controller(){
-  float d0x = -0.5;
-  float d0y = 0.866;
-  float d1x = 0.5;
-  float d1y = 0.866;
-  float d2x = 1;
-  float d2y = 0;
-  int gpx = gamepad1.left_joystick_x;
-  int gpy = gamepad1.left_joystick_y;
-  motor0.SetPower(d0x * gpx + d0y * gpy)
-  motor1.SetPower(d1x * gpx + d1y * gpy)
-  motor2.SetPower(d2x * gpx + d2y * gpy)
+  double d0x = -0.5;
+  double d0y = 0.866;
+  double d1x = 0.5;
+  double d1y = 0.866;
+  double d2x = 1;
+  double d2y = 0;
+  float gpx = gamepad1.left_stick_x;
+  float gpy = gamepad1.left_stick_y;
+  motor0.setPower(d0x * gpx + d0y * gpy)
+  motor1.setPower(d1x * gpx + d1y * gpy)
+  motor2.setPower(d2x * gpx + d2y * gpy)
 }
